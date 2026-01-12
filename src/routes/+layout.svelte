ccac0dca6b00c69d9a75e8eee98c1cea5d486cb0
Juan Altamira <juanpabloaltamira@protonmail.com>
2026-01-12 20:06:29 -0300

    Mobile: reduce main container padding for wider content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -55,5 +55,5 @@
 			</div>
 		</header>
 	{/if}
-	<main class="mx-auto max-w-6xl px-4 py-8">{@render children()}</main>
+	<main class="mx-auto max-w-6xl px-2 sm:px-4 py-6 sm:py-8">{@render children()}</main>
 </div>
